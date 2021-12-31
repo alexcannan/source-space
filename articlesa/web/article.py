41c17a5bbc5c8d6fed85310cd46f2f9d2101301e
Alex Cannan <alexfcannan@gmail.com>
2021-12-30 21:26:09 -0500

    home page and websocket tinkering

--- articlesa/web/article.py
+++ articlesa/web/article.py
@@ -1,31 +1,51 @@
 import asyncio
+import json
 from pathlib import Path
+from urllib.parse import unquote_plus
 
 from markupsafe import Markup
 from fastapi import FastAPI, Request, WebSocket
 from fastapi.responses import HTMLResponse
-from jinja2 import Template, Environment, FileSystemLoader, escape
+from jinja2 import Environment, FileSystemLoader, escape
 
 from articlesa.logger import logger
 
+from articlesa.core.find_source import recursive_source_check
 import articlesa.web.home
+
 
 app = FastAPI(title='Article Source Aggregator')
 
 app.include_router(articlesa.web.home.router)
 
-@app.get("/a/{article_url}", response_class=HTMLResponse)
-async def clip(article_url: str, request: Request):
+
+@app.get("/a/{article_url:path}", response_class=HTMLResponse)
+async def article(article_url: str, request: Request, depth: int=2):
     env = Environment(loader=FileSystemLoader(Path(__file__).parent))
     template = env.get_template('article.html')
 
-    return HTMLResponse(template.render(title=request.url.path[1:]))
+    article_url = unquote_plus(article_url)
+
+    return HTMLResponse(template.render(title=f"analysis of {article_url}"))
 
 
-@app.websocket("/ws/{article_url}")
-async def clip_websocket(websocket: WebSocket):
+@app.websocket("/ws/a/{article_url:path}")
+async def article_websocket(websocket: WebSocket):
+    # TODO: why isn't this being entered?
     await websocket.accept()
     article_url = websocket.path_params['article_url']
+
+    websocket.send_text("console.log('hello from ws')")
+
+    article_url = unquote_plus(article_url)
+
+    G = recursive_source_check(article_url, max_level=2)
+    logger.info(G.nodes)
+    logger.info(G.edges)
+
+    websocket.send_text(f"""
+        chart = ForceGraph(nodes: {json.dumps([G.nodes])}, edges: {json.dumps([G.edges])})
+    """)
 
     while True:
         data = await websocket.receive_text()
--- articlesa/web/home.py
+++ articlesa/web/home.py
@@ -1,4 +1,32 @@
+from pathlib import Path
+
 from fastapi import APIRouter, Request, WebSocket
+from fastapi.responses import HTMLResponse
+from jinja2 import Environment, FileSystemLoader, escape
+
+from articlesa.logger import logger
+
 
 router = APIRouter()
+
+
+@router.get("/", response_class=HTMLResponse)
+async def article(request: Request):
+    env = Environment(loader=FileSystemLoader(Path(__file__).parent))
+    template = env.get_template('article.html')
+
+    logger.info(f"hello from home")
+
+    return HTMLResponse(template.render(title="Article Source Aggregator"))
+
+
+@router.websocket("/ws/")
+async def home_websocket(websocket: WebSocket):
+    await websocket.accept()
+
+    logger.info(f"hello from home websocket")
+
+    while True:
+        data = await websocket.receive_text()
+        logger.info("got command: {}", data)
 
