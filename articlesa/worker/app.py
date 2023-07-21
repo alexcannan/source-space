06778e0827b6fe18cf3c3538ff70c6618d368c4c
Alex Cannan <alexfcannan@gmail.com>
2023-07-20 22:41:09 -0400

    articlesa.config; catch celery warnings

--- articlesa/worker/app.py
+++ articlesa/worker/app.py
@@ -1,22 +1,24 @@
 """ Celery configuration; uses custom AsyncIOPool. """
-import os
+
+import warnings
 
 from celery import Celery
 import celery_aio_pool as aio_pool
 
+from articlesa.config import CeleryConfig
 
-broker_url = os.environ.get("CELERY_BROKER_URL", "redis://localhost:6379/0")
-result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://localhost:6379/0")
-
-if aio_pool.patch_celery_tracer() is True:
-    pass
-else:
-    raise RuntimeError("celery_aio_pool patch failed")
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore")
+    if aio_pool.patch_celery_tracer() is True:
+        pass
+    else:
+        raise RuntimeError("celery_aio_pool patch failed")
 
 app = Celery(
     "articlesa",
-    broker=broker_url,
-    backend=result_backend,
+    broker=CeleryConfig.broker_url,
+    backend=CeleryConfig.result_backend,
     worker_pool=aio_pool.AsyncIOPool,
 )
+
 app.autodiscover_tasks(["articlesa.worker.parse"], force=True)
